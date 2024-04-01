f429b1b5ec94705abef616aad8cd236c21330641
Cristian Neumann <99703152+Cris-Neumann@users.noreply.github.com>
2024-04-01 17:31:12 -0300

    Update etl_data_warehouse.py

--- pipeline/etl_data_warehouse.py
+++ pipeline/etl_data_warehouse.py
@@ -22,3 +22,30 @@ def copy_insert_tables(conn:psycopg2.extensions.connection, cur:psycopg2.extensi
         cur.execute(query)
         conn.commit()
     return None
+
+def test_tables(conn:psycopg2.extensions.connection, test_queries:list):
+    """ Test Data Warehouse tables for data records.
+    Args:
+        conn (psycopg2.extensions.connection): Connection to Postgres DB.
+        test_queries (list): List of queries to execute.
+    Returns:
+        None
+    """
+    for query in test_queries:
+        data = pd.read_sql(query, conn)
+        print(data)
+    return None
+
+def main():
+    config = configparser.ConfigParser()
+    config.read('dwh.cfg')
+    conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
+    cur = conn.cursor()
+    copy_insert_tables(cur, conn, copy_table_queries)
+    copy_insert_tables(cur, conn, insert_dim_queries)
+    copy_insert_tables(cur, conn, insert_fact_queries)
+    test_tables(conn, test_queries)
+    conn.close()
+
+if __name__ == "__main__":
+    main()
