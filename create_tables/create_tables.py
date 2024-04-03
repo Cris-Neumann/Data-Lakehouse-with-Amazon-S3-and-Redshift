7e0cb5bf3f379ec713115ad1b1285bf34db0114d
Cristian Neumann <99703152+Cris-Neumann@users.noreply.github.com>
2024-04-02 23:02:38 -0300

    Update create_tables.py

--- create_tables/create_tables.py
+++ create_tables/create_tables.py
@@ -8,10 +8,9 @@ os.chdir(path)
 sys.path.append(path)
 from sql_queries.queries_create_tables import drop_table_queries, create_table_queries
 
-def create_drop_tables(conn:psycopg2.extensions.connection, cur:psycopg2.extensions.cursor, queries_list:list):
+def create_drop_tables(cur:psycopg2.extensions.cursor, queries_list:list):
     """ Perform operations on tables (create or drop) from a list of queries.
     Args:
-        conn (psycopg2.extensions.connection): Connection to Postgres DB.
         cur (psycopg2.extensions.cursor): Cursor to send commands to Postgres DB.
         queries_list (list): List of queries to execute.
     Returns:
@@ -19,7 +18,6 @@ def create_drop_tables(conn:psycopg2.extensions.connection, cur:psycopg2.extensi
     """
     for query in queries_list:
         cur.execute(query)
-        conn.commit()
     return None
 
 def main():
@@ -27,8 +25,9 @@ def main():
     config.read('dwh.cfg')
     conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
     cur = conn.cursor()
-    create_drop_tables(cur, conn, drop_table_queries)
-    create_drop_tables(cur, conn, create_table_queries)
+    create_drop_tables(cur, drop_table_queries)
+    create_drop_tables(cur, create_table_queries)
+    conn.commit()
     conn.close()
 
 if __name__ == "__main__":
