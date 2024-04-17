95f630e9807556204d24a1824f6d5125f43b2b4f
Cristian Neumann <99703152+Cris-Neumann@users.noreply.github.com>
2024-04-17 17:21:24 -0400

    Create aux_file.py

---
+++ utilities/aux_file.py
@@ -0,0 +1,9 @@
+yelp_files_names = ['business', 'review', 'tip', 'user'] 
+keys_to_remove = {'business':['categories', 'hours'],
+    'user':['useful', 'funny', 'cool', 'elite', 'friends',
+    'fans', 'compliment_hot', 'compliment_more',
+    'compliment_profile', 'compliment_cute',
+    'compliment_list', 'compliment_note',
+    'compliment_plain', 'compliment_cool',
+    'compliment_funny', 'compliment_writer',
+    'compliment_photos']}
