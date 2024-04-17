ac9688d68f08fed2de0531b47cc4299f3239227b
Cristian Neumann <99703152+Cris-Neumann@users.noreply.github.com>
2024-04-17 17:07:05 -0400

    Update insert_data_lake.py

--- pipeline/insert_data_lake.py
+++ pipeline/insert_data_lake.py
@@ -1,5 +1,11 @@
+import os
+import sys
 import json
-import boto3
+
+path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+os.chdir(path)
+sys.path.append(path)
+from utilities.connect_to_s3 import insert_into_s3
 
 def json_to_dict(json_path:str) -> json:
     """Transform yelp json to matrix json
@@ -20,35 +26,17 @@ def json_to_dict(json_path:str) -> json:
     data_extract = '\n'.join([json.dumps(obj) for obj in data_extract])
     return data_extract
 
-def insert_into_s3(yelp_data:json, s3_file_name:str) -> None:
-    """Insert yelp data into Amazon S3.
-    Args:
-        yelp_data (json): Data to insert into S3.
-        s3_file_name (str): Name of the file to insert into S3.
-    Returns:
-        None
-    """
-    s3_client = boto3.client('s3', 
-        aws_access_key_id='YOUR_ACCESS_KEY', 
-        aws_secret_access_key='YOUR_SECRET_ACCESS_KEY', 
-        region_name='YOUR_REGION')
-    s3_client.put_object(
-        Body=yelp_data,
-        Bucket='streaming-bucket-1',
-        Key=f'raw_yelp_files/{s3_file_name}.json')
-    return None
-
 def iter_yelp_files() -> None:
     """Iterate yelp files to insert into S3.
     Returns:
         None
     """
-    directory_path = r'YOUR_RAW_DIRECTORY_PATH'
+    directory_path = r'D:\GIT\yelp_dataset'
     yelp_files_names = ['business', 'checkin', 'review', 'tip', 'user']
     for file_name in yelp_files_names:
         file_path = f'{directory_path}\\yelp_academic_dataset_{file_name}.json'
         yelp_data = json_to_dict(file_path)
-        insert_into_s3(yelp_data, file_name)
+        insert_into_s3(yelp_data, file_name, 'raw_yelp_files')
     return None
 
 def main():
