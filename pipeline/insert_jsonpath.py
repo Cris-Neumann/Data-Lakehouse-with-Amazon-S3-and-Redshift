91e643f949754f3627a6f7eb1feaee9ad9f3a757
Cristian Neumann <99703152+Cris-Neumann@users.noreply.github.com>
2024-04-17 22:35:20 -0400

    Update insert_jsonpath.py

--- pipeline/insert_jsonpath.py
+++ pipeline/insert_jsonpath.py
@@ -1,36 +1,26 @@
+import os
+import sys
 import json
-import boto3
 
-def insert_into_s3(jsonpath_data:json, s3_file_name:str) -> None:
-    """Insert jsonpath data into Amazon S3.
-    Args:
-        jsonpath_data (json): Data to insert into S3.
-        s3_file_name (str): Name of the file to insert into S3.
-    Returns:
-        None
-    """
-    s3_client = boto3.client('s3', 
-        aws_access_key_id='YOUR_ACCESS_KEY', 
-        aws_secret_access_key='YOUR_SECRET_ACCESS_KEY', 
-        region_name='YOUR_REGION')
-    s3_client.put_object(
-        Body=json.dumps(jsonpath_data),
-        Bucket='streaming-bucket-1',
-        Key=f'yelp_jsonpath_files/{s3_file_name}.json')
-    return None
+path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+os.chdir(path)
+sys.path.append(path)
+from utilities.connect_to_s3 import insert_into_s3
 
 def iter_jsonpath_files() -> None:
     """Iterate jsonpath files to insert into S3.
     Returns:
         None
     """
-    directory_path = 'YOUR_JSONPATH_PATH'
-    jsonpath_files_names = ['business_jsonpath', 'review_jsonpath', 'tip_jsonpath', 'user_jsonpath']
+    directory_path = 'C:/Users/crist/Ubuntu20/data_lakehouse_with_redshift/json_path/'
+    jsonpath_files_names = ['business_jsonpath', 'review_jsonpath',
+                             'tip_jsonpath', 'user_jsonpath']
     for file_name in jsonpath_files_names:
         file_path = f'{directory_path}{file_name}.json'
         with open(file_path, 'r', encoding='utf-8') as open_file:
             jsonpath = json.load(open_file)
-            insert_into_s3(jsonpath, file_name)
+            json_data = json.dumps(jsonpath)
+            insert_into_s3(json_data, file_name, 'yelp_jsonpath_files')
     return None
 
 def main():
