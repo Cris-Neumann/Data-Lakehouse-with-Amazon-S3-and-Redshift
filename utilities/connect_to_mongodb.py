396612d5bba88a5459afedc778bcf9e837699265
Cristian Neumann <99703152+Cris-Neumann@users.noreply.github.com>
2024-04-19 19:31:42 -0400

    Update connect_to_mongodb.py

--- utilities/connect_to_mongodb.py
+++ utilities/connect_to_mongodb.py
@@ -1,12 +1,8 @@
 import pymongo
 import json
-from bson import ObjectId
 
 client = pymongo.MongoClient('mongodb://localhost:27017/')
 db = client['yelp_db']
 collection = db['raw_business']
-data = collection.find()
-jsonl_data = '\n'.join([json.dumps({k: str(v) if isinstance(v, ObjectId) else v for k, v in doc.items()}) for doc in data])
-
-# Imprimir los primeros 2 caracteres para verificar
-print(jsonl_data[:2])
+json_array_data = list(collection.find({}, {'_id': 0}))
+json_row_data = '\n'.join([json.dumps(obj) for obj in json_array_data])
