0735d2f6127284e159e2cd7f4588b7ff681d935d
Cristian Neumann <99703152+Cris-Neumann@users.noreply.github.com>
2024-04-20 20:40:33 -0400

    Update connect_to_mongodb.py

--- utilities/connect_to_mongodb.py
+++ utilities/connect_to_mongodb.py
@@ -1,8 +1,15 @@
 import pymongo
-import json
 
-client = pymongo.MongoClient('mongodb://localhost:27017/')
-db = client['yelp_db']
-collection = db['raw_business']
-json_array_data = list(collection.find({}, {'_id': 0}))
-json_row_data = '\n'.join([json.dumps(obj) for obj in json_array_data])
+def donwload_from_MongoDB(db_name:str, collection_name:str) -> list:
+    """Download data from MongoDB.
+    Args:
+        db_name (str): Database name in MongoDB.
+        collection_name (str): Collection name in MongoDB.
+    Returns:
+        list_with_json (list): list of jsons with the MongoDB data.
+    """
+    client = pymongo.MongoClient('mongodb://localhost:27017/')
+    db = client[db_name]
+    collection = db[collection_name]
+    list_with_json = list(collection.find({}, {'_id': 0}))
+    return list_with_json
